5eb9cbed9797b1ae61cd124718fd945d4f882c4a
Miguel_Verdejo <maverdejop@gmail.com>
2017-10-20 16:54:15 +0200

    Initial commit

---
+++ app/src/main/java/com/example/miguel/galeria_relative_y_grid_layouts/ActivityGaleria.java
@@ -0,0 +1,13 @@
+package com.example.miguel.galeria_relative_y_grid_layouts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityGaleria extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_galeria);
+    }
+}
